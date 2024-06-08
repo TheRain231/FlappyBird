43ce837d31a73d802c36c0022bf8f36ae5611dfd
Андрей Степанов <therain3164@yandex.ru>
2024-06-08 13:01:31 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Андрей Степанов on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
